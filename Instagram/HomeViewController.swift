bb513b49b88615ab836666825b0483f4b5e872bc
Kazuya Uchiyama <ucchi.k@gmail.com>
2018-08-21 12:55:19 +0900

    課題手前まで完成

--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -7,13 +7,141 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var postArray: [PostData] = []
+    
+    //DatabaseのobserveEbentの登録状態を表す
+    var observing = false           //登録状態って何？
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //テーブルセルのタップを無効にする
+        tableView.allowsSelection = false
+        
+        let nib = UINib(nibName: "PostTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "Cell")
+        
+        //テーブルの高さをAutoLayoutで調整
+        tableView.rowHeight = UITableViewAutomaticDimension
+        //テーブル業の高さの概算値
+        tableView.estimatedRowHeight = UIScreen.main.bounds.width + 100     //heightじゃなくてwidth?正方形だから？
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {        //animatedってなんだっけ？
+        super.viewWillAppear(animated)
+        print("DEBUG_PRINT: viewWillAppear")
+        
+        if Auth.auth().currentUser != nil {
+            if self.observing == false {
+                //要素が追加されたらpostarrayに追加してtableviewを表示する
+                let postsRef = Database.database().reference().child(Const.PostPath)
+                postsRef.observe(.childAdded, with: { snapshot in
+                    print("DEBUG_PRINT: .childAddedイベントが発生しました。")
+                    
+                    //postdataクラスを生成して受け取ったデータを設定
+                    if let uid = Auth.auth().currentUser?.uid {
+                        let postData = PostData(snapshot: snapshot, myId: uid)      //snapshot, uidはどこからきている？
+                        
+                        //保持している配列からidが同じものを探す  これなんで？
+                        var index: Int = 0
+                        for post in self.postArray {    //postarrayはどこにある？
+                            if post.id == postData.id {
+                                index = self.postArray.index(of: post)!  //indexは配列？
+                                break
+                            }
+                        }
+                        
+                        //差し替えるために一度削除
+                        self.postArray.remove(at: index)
+                        
+                        //削除したところに更新すみのデータを追加
+                        self.postArray.insert(postData, at: index)
+                        
+                        //tableViewを再表示
+                        self.tableView.reloadData()
+                    }
+                    
+                })
+                
+                //DatabaseのobserveEventが上記コードにより登録されたため、trueに
+                observing = true
+            }
+        } else {
+            if observing == true {
+                //ログアウトを検出したら一旦テーブルをクリアしてオブザーバーを削除
+                //テーブルをクリア
+                postArray = []
+                tableView.reloadData()
+                //オブザーバーを削除
+                Database.database().reference().removeAllObservers()
+                
+                //databaseのobserveEbentが上記コードにより解除されたのでfalse
+                observing = false
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //セルを取得してデータを設定
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PostTableViewCell
+        cell.setPostData(postArray[indexPath.row])
+        
+        //セル内のボタンのアクションをソースコードで設定
+        cell.likeButton.addTarget(self, action: #selector(handleButton(_:forEvent:)), for: .touchUpInside)
+        
+        return cell
+    }
+    
+    //セル内のボタンがタップされた時に呼ばれるメソッド
+    @objc func handleButton(_ sender: UIButton, forEvent event: UIEvent) {
+        print("DEBUG_PRINT: likeボタンがタップされました")
+        
+        //タップされた時のインデックスを求める
+        let touch = event.allTouches?.first     //これは何？
+        let point = touch!.location(in: self.tableView)     //整数が入る？
+        let indexPath = tableView.indexPathForRow(at: point)    //これは？配列？
+        
+        //配列からタップされたインデックスのデータを取り出す
+        let postData = postArray[indexPath!.row]
+        
+        //Firebaseに保存するデータの準備
+        if let uid = Auth.auth().currentUser?.uid {
+            if postData.isLiked {
+                //すでにいいねを押していた場合は解除
+                var index = -1      //-1は仮？
+                for likeId in postData.likes {
+                    if likeId == uid {
+                        //解除するためにインデックスを保存
+                        index = postData.likes.index(of: likeId)!
+                        break
+                    }
+                }
+                postData.likes.remove(at: index)
+            } else {
+                postData.likes.append(uid)
+            }
+            
+            //増えたlikesをFirebaseに保存する
+            let postRef = Database.database().reference().child(Const.PostPath).child(postData.id!)
+            let likes = ["likes": postData.likes]
+            postRef.updateChildValues(likes)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
